3f93a57d0b454561efc84c5a0f462c255e381a91
segabaser <139810447+segabaser@users.noreply.github.com>
2023-09-22 14:02:30 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,53 +1,51 @@
-    main()
 
-    data = generate_random_data()
-def main():
+    public static void main(String[] args) {
+        }
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+        int[] data = new int[10];
 
-    return data
-    data = [random.randint(1, 100) for _ in range(10)]
+    }
+        }
+public class RandomDataGenerator {    data = generate_random_data()
 if __name__ == "__main__":
-import random
-        print(f"Random Number: {item}")
 
-    for item in data:
 def generate_random_data():
-def main():
-def generate_random_data():
-    data = generate_random_data()
-
-
-    for item in data:
-import random
-
-
-    return data
-    main()
-if __name__ == "__main__":
     data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-
     main()
 import random
-if __name__ == "__main__":
-def generate_random_data():
-
-def main():
-    for item in data:
     return data
         print(f"Random Number: {item}")
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    for item in data:
+def main():
+
+    for item in data:
+def main():
+if __name__ == "__main__":
+    return data
+import random
+
+    main()
+
+        print(f"Random Number: {item}")
     data = [random.randint(1, 100) for _ in range(10)]
 
 def generate_random_data():
+    data = generate_random_data()if __name__ == "__main__":
     for item in data:
-
-        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    data = generate_random_data()
 def main():
 
-import random
+
     main()
     return data
 
-if __name__ == "__main__":
-    data = generate_random_data()
+def generate_random_data():
+        print(f"Random Number: {item}")
